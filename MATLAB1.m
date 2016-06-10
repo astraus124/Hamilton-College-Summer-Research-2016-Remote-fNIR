4e07a0d29c323c11f7059da6c880072482511424
astraus124 <astraus@hamilton.edu>
2016-06-10 15:21:09 +0000

    Add files via upload

---
+++ MATLAB1.m
@@ -0,0 +1,41 @@
+a=1
+
+sin(a)
+
+b = 2;
+
+A = magic(5),  B = ones(5) * 4.7;  C = A./B %Commments
+
+who
+
+whos
+
+%clear A
+%clear
+
+format long
+x = 7 + 10/3 + 5 ^ 1.2
+
+format short
+x = 7 + 10/3 + 5 ^ 1.2
+
+format bank
+daily_wage = 177.45;
+weekly_wage = daily_wage * 6
+
+format short e
+4.678 * 4.9
+
+r = [7 8 9 10 11];
+t = [2, 3, 4, 5, 6];
+res = r + t
+
+r = [7 8 9 10 11];
+t = [2, 3, 4, 5, 6];
+res = r + t
+
+m = [1 2 3; 4 5 6; 7 8 9]
+
+
+format rat
+4.678 * 4.9
